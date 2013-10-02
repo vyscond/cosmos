0157965c6003d08d8abae4f54520f6efbe08700b
vyscond <vyscond@gmail.com>
2013-10-02 13:47:20 -0300

    more commits

--- middleware/src/cosmos.py
+++ middleware/src/cosmos.py
@@ -7,7 +7,6 @@ class Planet :
     # Master Host Abstraction
     
     '''
-
     Orbit File Template
 
     {
@@ -23,26 +22,124 @@ class Planet :
 
     '''
 
-    def __init__( self , orbit_file ): # orbit file is a JSON formatted file :)
+    def __init__( self , orbit_file_path ): # orbit file is a JSON formatted file :)
         
-        # loading orbit configs
-        orbit_json = json.load( open( orbit_file ) )
+        # --- loading orbit configs - [bgn]
         
-        self.name = orbit_file["name"]
-        self.ip   = orbit_file["ip"]
-        self.port = int( orbit_file["port"] ) # just make sure, ok?
+        logging.info( "loading orbit from : " + orbit_file_path )
         
-        for moon_spec in orbit_file["moons"] :
+        orbit_json = json.load( open( orbit_file_path ) )
+        
+        self.name = orbit_json["name"]
+        
+        self.ip   = orbit_json["ip"]
+        
+        self.port = int( orbit_json["port"] ) # just make sure, ok?
+        
+        self.orbit = {}
+        
+        for moon in orbit_json["moons"] :
+            
+            self.orbit[ moon_spec["name"] ] = Moon( moon )
+            
+        # --- loading orbit configs - [end]
+    
+    def launch_expeditions( self , task_request_list , moon_name_list=None ):
+        
+        # --- Walktrough ---
+        # 
+        # 1. Check if user want only few moons on the orbi to get the job done
+        #
+        #   1.1 - moon_name_list = None 
+        #
+        #        User want to use all available moons
+        #
+        #   1.2 - moon_name_list = []
+        #        
+        #        We Have to select specific moons
+        #
+        # 2. Converting the LIST of TaskRequests on a Thread/Process Safe QUEUE
+        #
+        # 3. Build a Thread/Process Safe QUEUE for the TaskResponses
+        #
+        # 4. Build the Expedition
+        #
+        # 5. Launch the Expedition
+        #
+        # 6. Loop on Checking if all Expeditions are done
+        #
+        # 7. Return TaskResponses QUEUE
+        
+        logging.info( "launching expeditions" )
+        
+        logging.debug( "checking moon list" )
+        
+        # ---[ 1 ]
+         
+        working_moons = []
+        
+        if not moon_list :
+            
+            logging.debug( "using all available moons on the orbit" )
+            
+            self.working_moons = self.orbit.values()
+            
+        else :
+            
+            logging.debug( "using selection from user" )
+            
+            self.working_moons = [ self.orbit[moon_name] for moon_name in moon_name_list ]
+            
+        loggin.info( "enqueue tasks" )
+        
+        # ---[ 2 ]
+        
+        task_request_q  = Queue()
+        
+        for task_request in task_request_list : task_request_q.put( task_request ) 
+        
+        # ---[ 3 ]
+        
+        task_response_q = Queue()
+        
+        logging.info( "building expedition process" )
+        
+        # ---[ 4 ]
+        
+        running_expeditions = [ Expedition( task_request_q , task_response_q , moon ) for moon in working_moons ]
+        
+        logging.info( "starting up expeditions processes" )
+        
+        # ---[ 5 ]
+        
+        for expedition in running_expeditions : expedition.start()
+        
+        # ---[ 6 ]
+        
+        while True:
+            
+            if sum( [ expedition.is_alive() for expedition in running_expeditions ] ) == 0 : break
+         
+        # ---[ 7 ]
+         
+        return task_response_q
 
 class Moon :
     
-    def __init__( self , name , ip , port ):
+    def __init__( self , moon_spec ):
         
-        self.name = name 
+        moon_json = json.loads( moon )
+        
+        self.name = moon_json["name"]
+        self.ip   = moon_json["ip"]
+        self.port = int(  moon_json["port"] )
+        self.connection_tuple =  ( self.ip , self.port )
+        
+
+class MoonServer :
+    
+    def __init__( self , ip , port , application_index ):
+        
         self.ip   = ip
+        
         self.port = port
-        
-    def connection_tuple( self ):
-        
-        return ( self.ip , self.port )
-        
