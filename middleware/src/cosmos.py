00035f62a289e38c33b172b2c3edbed007b28e55
vyscond <vyscond@gmail.com>
2013-10-02 11:42:31 -0300

    more commits

--- middleware/src/cosmos.py
+++ middleware/src/cosmos.py
@@ -5,10 +5,44 @@ import json
 class Planet :
     
     # Master Host Abstraction
+    
+    '''
+
+    Orbit File Template
+
+    {
+        "name" : "planet_name",
+        "ip"   : "planet_ip",
+        "port" : planet_port,
+
+        "moons" : 
+        [
+            { "name" : "moon_name" , "ip" : "moon_ip" , "port" : moon_host } 
+        ]
+    }
+
+    '''
 
     def __init__( self , orbit_file ): # orbit file is a JSON formatted file :)
         
         # loading orbit configs
         orbit_json = json.load( open( orbit_file ) )
         
+        self.name = orbit_file["name"]
+        self.ip   = orbit_file["ip"]
+        self.port = int( orbit_file["port"] ) # just make sure, ok?
+        
+        for moon_spec in orbit_file["moons"] :
 
+class Moon :
+    
+    def __init__( self , name , ip , port ):
+        
+        self.name = name 
+        self.ip   = ip
+        self.port = port
+        
+    def connection_tuple( self ):
+        
+        return ( self.ip , self.port )
+        
