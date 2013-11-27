e9236bb589fdda3064eb46b1a00a845e8394d652
vyscond <vyscond@gmail.com>
2013-11-27 18:04:44 -0300

    more commits

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,6 +2,8 @@ moon_service="[Unit]\nDescription=Moon Server\nAfter=network.target\n\n[Service]
 
 moon_cfg="{\n    \n    \"name\" : \"$1\" ,\n    \"ip\"   : \"$2\" ,\n    \"port\" : \"$3\" ,\n    \"pid\"  : \"$4/moon/pid\" ,\n    \"stream\" : \n    {        \"stdin\"  : \"default\" ,\n        \"stdout\" : \"$4/moon/moonserver.std\" ,\n        \"stderr\" : \"$4/moon/moonserver.std\"\n    }\n    \n    ,\n    \n    \"directorys\" :\n    {\n        \"applications\"     : \"$4/moon/apps\" ,\n        \"application_profiles\" : \"$4/moon/app_profiles\"\n    }\n\n}\n"
 
+strawberry_node="Description='A basic static ethernet connection'\nInterface=eth0\nConnection=ethernet\nIP=static\nAddress=('$2')\nSkipNoCarrier=yes\n"
+
 echo '[cosmos]['`pwd`'][creating folder hierarchy]'
 
 echo '[cosmos]['`pwd`'][creating moon folder]'
@@ -24,6 +26,19 @@ touch moon.cfg
 echo '[cosmos]['`pwd`'][filling up moon.cfg with]'
 echo "echo -e \$moon_cfg"
 echo -e $moon_cfg > $4/moon/moon.cfg
+
+# - Configuring network
+
+cd /etc/netctl/
+echo '[cosmos]['`pwd`'][enabling moon service at boot]'
+
+echo "echo -e \$strawberry_node > strawberry_node"
+echo -e $strawberry_node > strawberry_node
+
+echo "netctl enable strawberry_node"
+netctl enable strawberry_node
+
+# - Daemon service
 
 cd /usr/lib/systemd/system/
 echo '[cosmos]['`pwd`'][installing moon.service file]'
